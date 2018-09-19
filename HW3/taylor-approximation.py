be179fcee2c228a3ae62f481285232bc0f8f834e
Brandon Willis <bwmusiclover1212@gmail.com>
2018-09-18 21:20:33 -0500

    Add output and make it clearer where to add input

--- HW3/taylor-approximation.py
+++ HW3/taylor-approximation.py
@@ -4,6 +4,13 @@ import numpy as np
 
 x = sy.Symbol('x')
 
+# Modify these variables only
+f = (1-x)**-1
+center = 0
+degree = 6
+value = 0.5
+# Modify these variables only
+
 def taylor(function, x0, n):
     i = 0
     p = 0
@@ -12,13 +19,10 @@ def taylor(function, x0, n):
         i += 1
     return p
 
-f = (1-x)**-1
-center = 0
-degree = 6
-value = 0.5
-
 absolute = f.subs(x, value)
 taylor = taylor(f, center, degree).subs(x, value)
 
+print("Actual value: ", absolute)
+print("Taylor approximation: ", taylor)
 print("Absolute error: ", math.fabs(absolute - taylor))
 print("Relative error: ", math.fabs((absolute - taylor) / absolute))
